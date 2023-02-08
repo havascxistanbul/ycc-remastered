85f917f8a7de026fa1ba99a4a60dc0642cdd7697
barankandil <barankandil10@gmail.com>
2023-02-08 14:11:52 +0300

    add Main layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
-import { Empty } from "../../componenets";
+import React from 'react';
+import { Navbar } from '../../componenets';
+import { Main } from '../../layout';
 
-function Home(): JSX.Element {
-    return (
-        <Empty />
-    )
+function Home (): JSX.Element {
+  return (
+    <Main>
+      <Navbar />
+    </Main>
+  );
 }
 
 export default Home;
