8ba71cc0b5b36a73956a2aeda08537608c39d8a8
BaranKandil <barankandil10@gmail.com>
2023-02-14 13:36:23 +0300

    Add Slider chevrons

--- src/componenets/Icons/index.ts
+++ src/componenets/Icons/index.ts
@@ -1,3 +1,5 @@
 export { default as RightChevron } from './RightChevron';
 export { default as DownChevron } from './DownChevron';
 export { default as UpChevron } from './UpChevron';
+export { default as SliderLeftChevron } from './SliderLeftChevron';
+export { default as SliderRightChevron } from './SliderRightChevron';
