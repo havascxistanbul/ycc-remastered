291e960468a41e6df906b05a3920fe38ebed6959
baran <barankandil10@gmail.com>
2023-02-15 16:38:13 +0300

    Remove unused props

--- src/componenets/LeftCol/LeftCol.tsx
+++ src/componenets/LeftCol/LeftCol.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-function LeftCol(props): JSX.Element {
+function LeftCol(): JSX.Element {
   return (
     <div className="left-col">
       <img
