7e1111fdc047f4e5be27f9fe15b5f2a0cf2520f8
BaranKandil <barankandil10@gmail.com>
2023-02-23 17:17:09 +0300

    Add filler component

--- src/pages/FAQ/FAQ.tsx
+++ src/pages/FAQ/FAQ.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Content, LeftCol, RightCol } from '../../componenets';
 import { Main } from '../../layout';
 import { data } from './data';
+import { AboutAnimation } from '../../componenets/Animations';
 
 function FAQ(): JSX.Element {
   return (
     <Main>
       <Content>
-        <LeftCol src={data.leftCol} />
+        <LeftCol>
+          <AboutAnimation />
+        </LeftCol>
         <RightCol
           header={data.rightCol.header}
           paragraph={data.rightCol.paragraph}
