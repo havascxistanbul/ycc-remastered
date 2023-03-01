4b8a9b05bc91c41b2c60087c2c97bfe15c408124
baran <barankandil10@gmail.com>
2023-03-01 22:29:02 +0300

    Add process page animation

--- src/componenets/Animations/index.ts
+++ src/componenets/Animations/index.ts
@@ -1,2 +1,3 @@
 export { default as HomeAnimation } from './Home';
 export { default as AboutAnimation } from './About';
+export { default as ProcessAnimation } from './Process';
--- src/pages/Process/Process.tsx
+++ src/pages/Process/Process.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Content, LeftCol, RightCol } from '../../componenets';
 import { Main } from '../../layout';
 import { data } from './data';
-import { AboutAnimation } from '../../componenets/Animations';
+import { ProcessAnimation } from '../../componenets/Animations';
 
 function Process(): JSX.Element {
   return (
     <Main>
       <Content>
         <LeftCol>
-          <AboutAnimation />
+          <ProcessAnimation />
         </LeftCol>
         <RightCol
           header={data.rightCol.header}
