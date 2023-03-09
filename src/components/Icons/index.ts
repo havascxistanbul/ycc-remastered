1fe50849f8c02c5fc60e88529f9621a37575ee70
baran <barankandil10@gmail.com>
2023-03-09 04:05:12 +0300

    Change color palette

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -3,3 +3,4 @@ export { default as DownChevron } from './DownChevron';
 export { default as UpChevron } from './UpChevron';
 export { default as SliderLeftChevron } from './SliderLeftChevron';
 export { default as SliderRightChevron } from './SliderRightChevron';
+export { default as Logo } from './Logo';
